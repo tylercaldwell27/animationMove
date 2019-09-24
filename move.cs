2bd5dff0e3380d1647ae9e1eda8d268c54edc269
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2019-09-23 22:58:15 -0400

    Create move.cs

---
+++ move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.Characters.FirstPerson;
+public class move : MonoBehaviour
+{
+    public Animator animator;
+    float speed = 0;
+    bool jump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+
+            speed = 1;
+            animator.SetFloat("speed", speed);
+        }
+      
+        else
+        {
+            speed = 0;
+            animator.SetFloat("speed", speed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            jump = true;
+            animator.SetBool("jump", true);
+        }
+        else
+        {
+            jump = false;
+            animator.SetBool("jump", false);
+        }
+        if (Input.GetButton("Fire1"))
+        {
+
+           // jump = true;
+            animator.SetBool("shoot", true);
+        }
+        else
+        {
+           // jump = false;
+            animator.SetBool("shoot", false);
+        }
+    }
+}
